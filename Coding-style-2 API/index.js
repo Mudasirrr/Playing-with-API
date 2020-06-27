767995004a7ba52c5c345e0604a499de0a567a5c
Mudasirrr <hmudasir714@email.com>
2020-06-27 22:02:41 +0500

    API use in JS with HTML

---
+++ Coding-style-2 API/index.js
@@ -0,0 +1,33 @@
+
+async function getData(){
+    const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+    console.log("response = ",response);
+    
+    const data = await response.json();
+    console.log("Data = ",data);
+}
+
+async function postData(){
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts',{
+        method: "POST",
+            body : JSON.stringify({
+                title: 'Chinar Stars',
+                body: 'Data',
+                userId: 4
+            }),
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        }
+    });
+    console.log("response = ",response);
+    
+    const data = await response.json();
+    console.log("Data = ",data);
+}
+
+
+getData();
+console.log("............POST..........")
+postData();
+
+
